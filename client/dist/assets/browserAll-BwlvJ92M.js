2dfdbbe3fa0e156f35dde0bdb0f28995012fb1c5
Lucas Sousa <ls442336@gmail.com>
2024-10-21 15:39:00 -0300

    Add new client build

--- client/dist/assets/browserAll-BwlvJ92M.js
+++
@@ -1,1 +0,0 @@
-import{e as i,A as e,C as a,a as s,E as t,F as n}from"./index-O9jmwYAn.js";import"./webworkerAll-BrhaF3p4.js";i.add(e);a.mixin(s);i.add(t);a.mixin(n);
---
+++ client/dist/assets/browserAll-DowpjFML.js
@@ -0,0 +1,1 @@
+import{e as i,A as e,C as a,a as s,E as t,F as n}from"./index-Dq9RI3b0.js";import"./webworkerAll-BmOb6NIu.js";i.add(e);a.mixin(s);i.add(t);a.mixin(n);
