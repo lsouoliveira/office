c2c5d3335ead76143b5f8a7ffd8ce0ba98197753
Lucas Sousa <ls442336@gmail.com>
2024-10-23 20:48:51 -0300

    Add new game build

--- client/dist/assets/browserAll-CSlkSdjo.js
+++
@@ -1,1 +0,0 @@
-import{e as i,A as e,C as a,a as s,E as t,F as n}from"./index-CVSx91_A.js";import"./webworkerAll-BhZ6mdfA.js";i.add(e);a.mixin(s);i.add(t);a.mixin(n);
---
+++ client/dist/assets/browserAll-iyYG-y_y.js
@@ -0,0 +1,1 @@
+import{e as i,A as e,C as a,a as s,E as t,F as n}from"./index-Okoi_DIP.js";import"./webworkerAll-BmHe5_xo.js";i.add(e);a.mixin(s);i.add(t);a.mixin(n);
